83eb6df004854625beb5a29b0f9eed1eefc42b1d
colder <cncolder@gmail.com>
2022-08-19 21:18:35 +0800

    feat: hello word

---
+++ docs/index.tsx
@@ -0,0 +1,4 @@
+import React from "https://esm.sh/react";
+import { createRoot } from "https://esm.sh/react-dom/client";
+
+createRoot(document.getElementById("root")!).render(<div>Hello World</div>);
