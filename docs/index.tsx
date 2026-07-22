4bb5dc0d85b09718386668ea90e5d8bf0804a362
colder <cncolder@gmail.com>
2026-07-23 00:13:12 +0800

    Remove obsolete Deno and docs setup files

--- docs/index.tsx
+++
@@ -1,4 +0,0 @@
-import React from "https://esm.sh/react";
-import { createRoot } from "https://esm.sh/react-dom/client";
-
-createRoot(document.getElementById("root")!).render(<div>Hello World</div>);
